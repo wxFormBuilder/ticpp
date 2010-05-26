c71f75aabb4f2925b027ab2eb99a1298e578d036
tim.dave <tim.dave@51f46dd9-6d1a-0410-b0a4-47219f4268eb>
2010-05-26 14:34:06 +0000

    More mock methods

--- trunk/tests/mocks/TicppMock.hh
+++ trunk/tests/mocks/TicppMock.hh
@@ -11,16 +11,17 @@ namespace ticpp
 		{
 			MOCK_METHOD0( Clear, void ( void ) );
 			MOCK_METHOD1( SetValue, void( const std::string& ) );
-			MOCK_METHOD1( InsertEndChild, void( const StrictMock< Element >& ) );
+			MOCK_METHOD1( InsertEndChild, void(  StrictMock< Element >& ) );
 			MOCK_METHOD1( LinkEndChild, void( const StrictMock< Node >* ) );
 			MOCK_METHOD1( LinkEndChild, void( const StrictMock< Element >* ) );
 			MOCK_METHOD1( SetText, void ( const double ) );
 			MOCK_METHOD1( SetText, void ( const std::string& ) );
 			MOCK_METHOD1( GetValue, void ( std::string* ) );
-			MOCK_CONST_METHOD0( GetText, std::string (void) );
 			MOCK_METHOD1( SetIntText, void ( const unsigned int ) );
 			MOCK_METHOD1( RemoveChild, void ( Node* ) );
+			MOCK_CONST_METHOD0( Clone, const StrictMock< Node >& ( void ) );		//This is done because auto_ptr's copy constructor is declared explicit and generates a compiler error inside googlemock.
 			MOCK_CONST_METHOD0( ToElement, StrictMock< Element >* (void) );
+			MOCK_CONST_METHOD0( NoChildren, bool (void) );
 			MOCK_CONST_METHOD0( FirstChild, StrictMock< Node >* (void ) );
 			MOCK_CONST_METHOD1( FirstChild, StrictMock< Node >* ( const std::string& ) );
 			MOCK_CONST_METHOD2( FirstChild, StrictMock< Node >* ( const std::string&, bool ) );
@@ -29,6 +30,12 @@ namespace ticpp
 			MOCK_CONST_METHOD0( FirstChildElement, StrictMock< Element >* () );
 			MOCK_CONST_METHOD1( FirstChildElement, StrictMock< Element >* ( bool ) );
 			MOCK_CONST_METHOD0( Value, std::string (void) );
+
+			//This is the work around for the Clone method not returning auto_ptr.
+			const Node* release( void ) const
+			{
+				return this;
+			}
 		};
 		
 		struct Element : public Node
@@ -37,8 +44,15 @@ namespace ticpp
 			{
 			}
 			MOCK_CONST_METHOD1( GetAttribute, std::string ( const std::string& ) );
+			MOCK_CONST_METHOD2( GetAttribute, std::string ( std::string, bool ) );
 			MOCK_METHOD2( SetAttribute, void ( const std::string&, const int ) );
 			MOCK_METHOD2( SetAttribute, void ( const std::string&, const std::string& ) );
+			MOCK_CONST_METHOD0( GetText, std::string (void) );
+
+			bool operator==( const Element& other) const
+			{
+				return *this == other;
+			}
 		};
 
 		struct Document : public Node
