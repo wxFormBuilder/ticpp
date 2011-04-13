3d2047bb9816520a132e9b35b6bd9fce1eca88bb
tim.dave <tim.dave@51f46dd9-6d1a-0410-b0a4-47219f4268eb>
2011-04-13 20:17:15 +0000

    Updated mocks

--- trunk/tests/mocks/TicppMock.hh
+++ trunk/tests/mocks/TicppMock.hh
@@ -5,44 +5,68 @@ namespace ticpp
 {
 	namespace mock
 	{
+		struct Node;
 		struct Element;
 		struct Declaration;
 
-		struct Node
+		struct NodeBase
 		{
+			NodeBase( void )
+			{
+			}
 			MOCK_METHOD0( Clear, void ( void ) );
 			MOCK_METHOD1( SetValue, void( const std::string& ) );
-			MOCK_METHOD1( InsertEndChild, void( const StrictMock< Element >& ) );
-			MOCK_METHOD1( LinkEndChild, void( const StrictMock< Node >* ) );
-			MOCK_METHOD1( LinkEndChild, void( const StrictMock< Element >* ) );
-			MOCK_METHOD1( LinkEndChild, void( const StrictMock< Declaration >* ) );
+			MOCK_METHOD1( InsertEndChild, void( const Node& ) );
+			MOCK_METHOD1( LinkEndChild, void( const Node* ) );
 			MOCK_METHOD1( SetText, void ( const double ) );
 			MOCK_METHOD1( SetText, void ( const std::string& ) );
 			MOCK_METHOD1( GetValue, void ( std::string* ) );
 			MOCK_METHOD1( SetIntText, void ( const unsigned int ) );
 			MOCK_METHOD1( RemoveChild, void ( Node* ) );
-			MOCK_CONST_METHOD0( Clone, const StrictMock< Node >& ( void ) );		//This is done because auto_ptr's copy constructor is declared explicit and generates a compiler error inside googlemock.
-			MOCK_CONST_METHOD0( ToElement, StrictMock< Element >* ( void ) );
+			MOCK_CONST_METHOD0( Clone, const Node& ( void ) );		//This is done because auto_ptr's copy constructor is declared explicit and generates a compiler error inside googlemock.
+			MOCK_CONST_METHOD0( ToElement, Element*( void ) );
 			MOCK_CONST_METHOD0( NoChildren, bool ( void ) );
-			MOCK_CONST_METHOD0( FirstChild, StrictMock< Node >* ( void ) );
-			MOCK_CONST_METHOD1( FirstChild, StrictMock< Node >* ( const std::string& ) );
-			MOCK_CONST_METHOD2( FirstChild, StrictMock< Node >* ( const std::string&, bool ) );
-			MOCK_CONST_METHOD1( IterateChildren, StrictMock< Node >* ( StrictMock< Node >* ) );
-			MOCK_CONST_METHOD2( IterateChildren, StrictMock< Node >* ( const std::string, StrictMock< Node >* ) );
-			MOCK_CONST_METHOD0( FirstChildElement, StrictMock< Element >* () );
-			MOCK_CONST_METHOD1( FirstChildElement, StrictMock< Element >* ( bool ) );
+			MOCK_CONST_METHOD0( FirstChild, Node*( void ) );
+			MOCK_CONST_METHOD1( FirstChild, Node*( const std::string& ) );
+			MOCK_CONST_METHOD2( FirstChild, Node*( const std::string&, bool ) );
+			MOCK_CONST_METHOD1( IterateChildren, Node*( Node* ) );
+			MOCK_CONST_METHOD2( IterateChildren, Node*( const std::string, Node* ) );
+			MOCK_CONST_METHOD0( FirstChildElement, Element*() );
+			MOCK_CONST_METHOD1( FirstChildElement, Element*( bool ) );
 			MOCK_CONST_METHOD0( Value, std::string( void ) );
+			virtual ~NodeBase( void )
+			{
+			}
+		};
 
+		struct Node : public StrictMock< NodeBase >
+		{
 			//This is the work around for the Clone method not returning auto_ptr.
 			const Node* release( void ) const
 			{
 				return this;
 			}
+
+			Node( void ) : StrictMock< NodeBase >()
+			{
+			}
+
+			Node( const Node& ) : StrictMock< NodeBase >()
+			{
+			}
+
+			bool operator==( const Node& other ) const
+			{
+				return *this == other;
+			}
+			virtual ~Node( void )
+			{
+			}
 		};
 
-		struct Element : public Node
+		struct ElementBase : public Node
 		{
-			Element( const std::string& ) : Node()
+			ElementBase( const std::string& ) : Node()
 			{
 			}
 			MOCK_CONST_METHOD1( GetAttribute, std::string( const std::string& ) );
@@ -51,19 +75,32 @@ namespace ticpp
 			MOCK_METHOD2( SetAttribute, void ( const std::string&, const std::string& ) );
 			MOCK_CONST_METHOD0( GetText, std::string( void ) );
 			MOCK_CONST_METHOD1( GetText, std::string( bool ) );
+			virtual ~ElementBase( void )
+			{
+			}
+		};
+
+		struct Element : public StrictMock< ElementBase >
+		{
+			explicit Element( const std::string& name = std::string() ) : StrictMock< ElementBase >( name )
+			{
+			}
 
 			bool operator==( const Element& other ) const
 			{
 				return *this == other;
 			}
-		};
-
-		struct Document : public Node
-		{
-			Document( void ) : Node()
+			virtual ~Element( void )
 			{
 			}
-			Document( const std::string& ) : Node()
+		};
+
+		struct DocumentBase : public Node
+		{
+			DocumentBase( void ) : Node()
+			{
+			}
+			DocumentBase( const std::string& ) : Node()
 			{
 			}
 			MOCK_METHOD0( SaveFile, void ( void ) );
@@ -71,27 +108,68 @@ namespace ticpp
 			MOCK_METHOD0( LoadFile, void ( void ) );
 			MOCK_METHOD1( LoadFile, void ( const std::string& ) );
 			MOCK_METHOD1( Parse, void ( const std::string& ) );
-			MOCK_CONST_METHOD0( GetDocument, StrictMock< Document >* ( void ) );
+
+			virtual ~DocumentBase( void )
+			{
+			}
 		};
-		
-		struct Declaration : public Node
+
+		struct Document : public StrictMock< DocumentBase >
 		{
-			Declaration( void )
+			Document( void ) : StrictMock< DocumentBase >()
 			{
 			}
-			Declaration( const std::string&, const std::string&, const std::string& )
+			Document( const std::string& name ) : StrictMock< DocumentBase >( name )
+			{
+			}
+			MOCK_CONST_METHOD0( GetDocument, Document*( void ) );
+			virtual ~Document( void )
 			{
 			}
 		};
-		
+
+		struct DeclarationBase : public Node
+		{
+			DeclarationBase( void ) : Node()
+			{
+			}
+
+			DeclarationBase( const std::string&, const std::string&, const std::string& ) : Node()
+			{
+			}
+			virtual ~DeclarationBase( void )
+			{
+			}
+		};
+
+		struct Declaration : public StrictMock< DeclarationBase >
+		{
+			Declaration( void ) : StrictMock< DeclarationBase >()
+			{
+			}
+
+			Declaration( const std::string& p1, const std::string& p2, const std::string& p3 ) : StrictMock< DeclarationBase >( p1, p2, p3 )
+			{
+			}
+			virtual ~Declaration( void )
+			{
+			}
+		};
+
 		struct Comment : public Node
 		{
+			Comment( void )
+			{
+			}
+			virtual ~Comment( void )
+			{
+			}
 		};
+
+		inline std::ostream& operator << ( std::ostream& os, const Node& )
+		{
+			return( os );
+		}
 	}
 }
-inline std::ostream& operator << ( std::ostream& os, const ticpp::mock::Node& )
-{
-	return( os );
-}
-
 #endif // !defined ( __TINY_XML_MOCK__ )
